ee29f2a3493662e47bd1925a599d7401430481f1
SanchitaMishraPython <149459076+SanchitaMishraPython@users.noreply.github.com>
2023-11-02 21:49:32 +0530

    Add files via upload

---
+++ fare_withdistance.py
@@ -0,0 +1,17 @@
+distance = float(input("Enter the distance in kilometers: "))
+if distance <= 50:
+        firstfare = distance * 8
+        print("Total fare is:",firstfare)
+elif distance <= 100:
+        secondfare = distance * 10
+        print("Total fare is:",secondfare)
+else:
+        thirdfare = distance * 12
+        print("Total fare is:",thirdfare)
+    
+
+
+
+
+
+
